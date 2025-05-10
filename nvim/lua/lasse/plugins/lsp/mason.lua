caca3d23a1dc8fbc4ee1d1e83e2e253ed8f4e2d8
Lasse <lass5588@icloud.com>
2025-05-10 15:15:13 +0200

    Fixed LSP by forcing mason-lspconfig to not upgrade to version 2.0.0

--- nvim/lua/lasse/plugins/lsp/mason.lua
+++ nvim/lua/lasse/plugins/lsp/mason.lua
@@ -1,8 +1,8 @@
 return {
 	"williamboman/mason.nvim",
 	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		{ "williamboman/mason-lspconfig.nvim", version = "1.32.0" },
+    { "WhoIsSethDaniel/mason-tool-installer.nvim", commit = "1255518cb067e038a4755f5cb3e980f79b6ab89c" },
 	},
 	config = function()
 		-- import mason
